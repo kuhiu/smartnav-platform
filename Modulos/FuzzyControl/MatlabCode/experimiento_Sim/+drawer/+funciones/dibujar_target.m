02908d37cb797f655ecb526b79529544a18e3856
Fuschetto97 <marfus@hotmail.es>
2022-05-16 13:17:16 -0300

    delete old nodes

--- Modulos/FuzzyControl/MatlabCode/experimiento_Sim/+drawer/+funciones/dibujar_target.m
+++
@@ -1,4 +0,0 @@
-function dibujar_target(target,radius)
-    rectangle('Position',[target(1)-0.5*radius target(2)-0.5*radius radius radius], 'Curvature',[1 1],'FaceColor','r','EdgeColor','r')
-end
-
--- Modulos/FuzzyControl/MatlabCode/experimientos2/showNavigationTrajectories.m
+++
@@ -1,6 +0,0 @@
-function showNavigationTrajectories(fis,obstacle,target,robot)
-
-% Show robot trajectories in the simulation environments.
-drawEnvironmentAndShowTrajectory(obstacle,target,robot,'Navigation Task',fis)
-
-end
