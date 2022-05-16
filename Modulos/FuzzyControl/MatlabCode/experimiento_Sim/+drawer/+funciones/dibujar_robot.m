afd8091a1aa1c2cefb1ebb8492c92049f774d327
Fuschetto97 <marfus@hotmail.es>
2022-05-16 13:17:16 -0300

    delete old nodes

--- Modulos/FuzzyControl/MatlabCode/experimiento_Sim/+drawer/+funciones/dibujar_robot.m
+++
@@ -1,4 +0,0 @@
-function dibujar_robot(robot,radius,p)
-    rectangle('Position',[robot(1)-0.5*radius robot(2)-0.5*radius radius radius], 'Curvature',[1 1],'FaceColor','y','EdgeColor','y');
-end
-
--- Modulos/ImgProcessing/README.md
+++
@@ -1,3 +0,0 @@
-# Img Processing
-
-The img processing codebase has moved to a [stand alone github repository](https://github.com/kuhiu/SmartNavSys-ImgProc).
