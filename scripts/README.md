9edb8e782089f46e7d09bdaa71f22dd5d6272a04
Fuschetto97 <marfus@hotmail.es>
2022-06-19 21:34:48 -0300

    add script to install all drivers on startup

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+# Install drivers
+
+* Save installDrivers.sh in /etc/init.d and chmod +x
+* Save installDrivers.service in /etc/systemd/system/
+* systemctl enable installDrivers
---
+++ scripts/installDrivers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "InstallDrivers script"
+sudo insmod /home/ubuntu/drivers/driver_encoder_EMIOgpio_PL_1.ko;
+sudo insmod /home/ubuntu/drivers/driver_encoder_EMIOgpio_PL_2.ko;
+sudo insmod /home/ubuntu/drivers/driver_buttons_EMIOgpio_PL_1.ko;
+sudo insmod /home/ubuntu/drivers/driver_leds_EMIOgpio_PL.ko;
+sudo insmod /home/ubuntu/drivers/driver_i2c_PS_ov7670.ko;
+sudo insmod /home/ubuntu/drivers/driver_vdma.ko;
+sudo insmod /home/ubuntu/drivers/driver_MIOgpio_PS.ko;
+sudo insmod /home/ubuntu/drivers/driver_pwm_EMIOgpio_PL.ko;
+sudo insmod /home/ubuntu/drivers/driver_EMIOgpio_PL.ko;
+sudo insmod /home/ubuntu/drivers/driver_i2c_PS_hmc5883L.ko; 
+sudo rmmod  /home/ubuntu/drivers/driver_i2c_PS_hmc5883L.ko;
+sudo /home/ubuntu/apps/ControlVdma;
