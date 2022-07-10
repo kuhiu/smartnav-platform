14a2d08ca02c9a6a3091adf683a94beb8f773b62
kuhiu <marfus@hotmail.es>
2022-07-10 19:45:05 -0300

    update scripts

--- scripts/installDrivers.sh
+++ scripts/installDrivers.sh
@@ -8,10 +8,12 @@ sudo insmod /home/ubuntu/drivers/driver_leds_EMIOgpio_PL.ko;
 sudo insmod /home/ubuntu/drivers/driver_i2c_PS_ov7670.ko;
 sudo insmod /home/ubuntu/drivers/driver_vdma.ko;
 sudo insmod /home/ubuntu/drivers/driver_MIOgpio_PS.ko;
-sudo insmod /home/ubuntu/drivers/driver_pwm_EMIOgpio_PL.ko;
+sudo insmod /home/ubuntu/drivers/driver_pwm_EMIOgpio_PL_1.ko;
+sudo insmod /home/ubuntu/drivers/driver_pwm_EMIOgpio_PL_2.ko;
 sudo insmod /home/ubuntu/drivers/driver_EMIOgpio_PL.ko;
 sudo insmod /home/ubuntu/drivers/driver_i2c_PS_hmc5883L.ko; 
 sudo rmmod  /home/ubuntu/drivers/driver_i2c_PS_hmc5883L.ko;
 sudo /home/ubuntu/apps/ControlVdma;
 
-sudo /home/ubuntu/buttonsControl/buttonsControl;
+echo "Run App"
+sudo nohup /home/ubuntu/buttonsControl/buttonsControl > /dev/null 2>&1 &
