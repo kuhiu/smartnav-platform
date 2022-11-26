bf2c16c79db985c3ba4e856520c992023dd05d86
kuhiu <marfus@hotmail.es>
2022-11-26 13:25:21 -0300

    update FrameProcessor to the new version of capture-frame module

--- platform/image/Image/project-spec/meta-user/recipes-apps/frame-processor/files/FrameProcessor.cpp
+++ platform/image/Image/project-spec/meta-user/recipes-apps/frame-processor/files/FrameProcessor.cpp
@@ -1,65 +1,25 @@
 #include "FrameProcessor.hpp"
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <chrono>
+#include <mutex>
 
-FrameProcessor::FrameProcessor() {
-  __is_running = true;
+void FrameProcessor::__processFrame(std::shared_ptr<VirtualImage> frame) {
+  // Start measuring time
+  auto begin = std::chrono::high_resolution_clock::now();
 
-  printf("Open vdma driver\n");
-  if ( (__vdma_fd = open("/dev/vdma_control_chardev", O_RDWR)) == -1)
-    throw std::runtime_error("Could not open vdma driver\n");
-
-  __processor_thread = std::thread(&FrameProcessor::__processFrame, this);
-}
-
-FrameProcessor::~FrameProcessor() {
-  __is_running = false;
-  __processor_thread.join();
-}
-
-void FrameProcessor::saveFrame(const VirtualImage &frame) {
-  std::unique_lock<std::mutex> frame_lock(__frame_guard);
-  if (__frame == nullptr) {
-    // Create a virtual image
-    __frame = std::make_shared<VirtualImage>(frame); 
-    // Manual unlocking is done before notifying, to avoid waking up
-    // the waiting thread only to block again (see notify_one for details)
-    frame_lock.unlock();
-    __cv.notify_one();
+  { // Run Person detector 
+    std::unique_lock<std::mutex> lock(__result_guard);
+    __results = __detector.detect(frame);
   }
+
+  // Stop measuring time and calculate the elapsed time
+  auto end = std::chrono::high_resolution_clock::now();
+  auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(end - begin);
+
+  std::cout << "Time recognition" << (elapsed.count()) << "seconds" << std::endl;
 }
 
 std::vector<RecognitionResult> FrameProcessor::getResults() { 
   std::unique_lock<std::mutex> lock(__result_guard);
   return __results; 
-}
-
-void FrameProcessor::__processFrame() {
-  while(__is_running) {
-    std::unique_lock<std::mutex> frame_lock(__frame_guard);
-    __cv.wait(frame_lock, [&] () {
-      return (__frame != nullptr);
-    });
-    __consumeFrame();
-    __frame = nullptr;
-  }
-}
-
-void FrameProcessor::__consumeFrame() {
-  // Start measuring time
-  auto begin = std::chrono::high_resolution_clock::now();
-  __frame->saveAsJpg("test2.jpg");
-
-  { // Run Person detector 
-    std::unique_lock<std::mutex> lock(__result_guard);
-    __results = __detector.detect(__frame);
-  }
-
-  // Stop measuring time and calculate the elapsed time
-  auto end = std::chrono::high_resolution_clock::now();
-  auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-
-  std::cout << "Time recognition" << (elapsed.count() * 1e-9) << "seconds" << std::endl;
 }
