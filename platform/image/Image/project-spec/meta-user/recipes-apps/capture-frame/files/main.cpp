b31a9213e22fdace2f5e4ba901533c245e614581
kuhiu <marfus@hotmail.es>
2022-11-26 13:29:36 -0300

    add test app for capture-frame module

--- platform/image/Image/project-spec/meta-user/recipes-apps/capture-frame/files/main.cpp
+++ platform/image/Image/project-spec/meta-user/recipes-apps/capture-frame/files/main.cpp
@@ -5,23 +5,20 @@
 #include "modules/Logger/Logger.hpp"
 
 void frameCallback(void* data, unsigned int size) {
-  printf("Entre a frame callbac, size: %d\n", size);
   VirtualImage rgb_image(640, 480, 3, ColorSpaces::RGB, data);
-  printf("Save as jpg\n");
-  rgb_image.saveAsJpg("testrgb.jpg");
+  rgb_image.resize(320, 320);
+  rgb_image.flip(false);
+  rgb_image.colorBalancing(1.0);
+  //rgb_image.saveAsJpg("testrgb.jpg");
 }
 
 int main() {
   Logger::log() << "Hello World Im testing CaptureFrame module";
 
-  CaptureFrame capture_frame(frameCallback, 640, 480, CaptureFrame::pixelFormat::PIX_FMT_RGB24, 4);
+  CaptureFrame capture_frame(frameCallback, 640, 480, CaptureFrame::pixelFormat::PIX_FMT_RGB24, 1);
   
-  printf("CaptureFrame was built\n");
   while (true) {
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
-
-
-  printf("Main: finish\n");
   return 0;
 }
