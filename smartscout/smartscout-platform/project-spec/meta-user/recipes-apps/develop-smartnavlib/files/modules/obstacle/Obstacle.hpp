eb340edf735bcddb7117b15593d61476d6f5a854
Kuhiu <marfus@hotmail.es>
2023-03-08 20:46:23 -0300

    the polar and cartesian positions were relocated

--- smartscout/smartscout-platform/project-spec/meta-user/recipes-apps/develop-smartnavlib/files/modules/obstacle/Obstacle.hpp
+++ smartscout/smartscout-platform/project-spec/meta-user/recipes-apps/develop-smartnavlib/files/modules/obstacle/Obstacle.hpp
@@ -1,42 +1,42 @@
 #ifndef BD4C42A3_637A_4380_9C17_15346BBBE48F
 #define BD4C42A3_637A_4380_9C17_15346BBBE48F
 
-#include <PositionEstimator.hpp>
+#include <CartesionPosition.hpp>
 
 class Obstacle {
 public:
   /** Obstacle constructor */
-  Obstacle(PositionEstimator::cartesianPosition position, PositionEstimator::cartesianPosition leftmost_point,
-      PositionEstimator::cartesianPosition rightmost_point) : 
+  Obstacle(CartesianPosition position, CartesianPosition leftmost_point,
+      CartesianPosition rightmost_point) : 
       __position(position), __leftmost_point(leftmost_point), __rightmost_point(rightmost_point) {};
   /** Obstacle destructor */
   ~Obstacle() = default;
   /**
    * @brief Get the Position object
    * 
-   * @return PositionEstimator::cartesianPosition 
+   * @return CartesianPosition 
    */
-  PositionEstimator::cartesianPosition getPosition() const { return __position; };
+  CartesianPosition getPosition() const { return __position; };
   /**
    * @brief Get the Leftmost Point object
    * 
    * @return Dimensions 
    */
-  PositionEstimator::cartesianPosition getLeftmostPoint() const { return __leftmost_point; };
+  CartesianPosition getLeftmostPoint() const { return __leftmost_point; };
   /**
    * @brief Get the Rightmost Point object
    * 
    * @return Dimensions 
    */
-  PositionEstimator::cartesianPosition getRightmostPoint() const { return __rightmost_point; };
+  CartesianPosition getRightmostPoint() const { return __rightmost_point; };
 
 private:
   /** Obstacle's position */
-  PositionEstimator::cartesianPosition __position;
+  CartesianPosition __position;
   /** Leftmost point of the obstacle's width */
-  PositionEstimator::cartesianPosition __leftmost_point;
+  CartesianPosition __leftmost_point;
   /** Rightmost point of the obstacle's width */
-  PositionEstimator::cartesianPosition __rightmost_point;
+  CartesianPosition __rightmost_point;
 
 };
 
