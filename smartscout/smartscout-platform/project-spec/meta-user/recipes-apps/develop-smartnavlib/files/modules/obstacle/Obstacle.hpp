0fc6e54e6c84e2aae9d6a3ad46a6df3247c5696e
Kuhiu <marfus@hotmail.es>
2023-03-09 12:40:53 -0300

    now the obstacle is created with the distance to the obstacle, the current angle and the current position

--- smartscout/smartscout-platform/project-spec/meta-user/recipes-apps/develop-smartnavlib/files/modules/obstacle/Obstacle.hpp
+++ smartscout/smartscout-platform/project-spec/meta-user/recipes-apps/develop-smartnavlib/files/modules/obstacle/Obstacle.hpp
@@ -2,13 +2,29 @@
 #define BD4C42A3_637A_4380_9C17_15346BBBE48F
 
 #include <CartesionPosition.hpp>
-
+#include <RecognitionResult.hpp>
 class Obstacle {
 public:
   /** Obstacle constructor */
-  Obstacle(CartesianPosition position, CartesianPosition leftmost_point,
-      CartesianPosition rightmost_point) : 
-      __position(position), __leftmost_point(leftmost_point), __rightmost_point(rightmost_point) {};
+  Obstacle(float curr_robot_angle, float distance_to_obstacle, CartesianPosition curr_position, RecognitionResult recognized_object) {
+        PolarPosition obstacle_polar(curr_robot_angle, distance_to_obstacle);
+        __position = PositionEstimator::polarToCartesian(obstacle_polar);
+        __position.x = __position.x + curr_position.x;
+        __position.y = __position.y + curr_position.y;
+        // Width of the obstacle to avoid 
+        // Leftmost point of the obstacle's width 
+        PolarPosition leftmost_obstacle_point_pol((atan2((0.5-recognized_object.xmin)*16, distance_to_obstacle)*180/M_PI) + curr_robot_angle, 
+            (std::sqrt(std::pow(distance_to_obstacle, 2) + std::pow((0.5-recognized_object.xmin)*16, 2))) );
+        __leftmost_point = PositionEstimator::polarToCartesian(leftmost_obstacle_point_pol);
+        __leftmost_point.x = __leftmost_point.x + curr_position.x;
+        __leftmost_point.y = __leftmost_point.y + curr_position.y;
+        // Rightmost point of the obstacle's width 
+        PolarPosition rightmost_obstacle_point_pol((atan2(-(recognized_object.xmax-0.5)*16, distance_to_obstacle)*180/M_PI) + curr_robot_angle, 
+            std::sqrt(std::pow(distance_to_obstacle, 2) + std::pow((recognized_object.xmax-0.5)*16, 2)));
+        __rightmost_point = PositionEstimator::polarToCartesian(rightmost_obstacle_point_pol);
+        __rightmost_point.x = __rightmost_point.x + curr_position.x;
+        __rightmost_point.y = __rightmost_point.y + curr_position.y;
+      };
   /** Obstacle destructor */
   ~Obstacle() = default;
   /**
